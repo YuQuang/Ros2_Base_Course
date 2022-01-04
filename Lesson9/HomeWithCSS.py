35c591f58453dc1b2a0af4d427fafb95ecf90aff
Quang Xu <Quang.Xu@ad.aimmalab.com>
2022-01-04 13:43:31 +0800

    Fix username GET

--- Lesson9/HomeWithCSS.py
+++ Lesson9/HomeWithCSS.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('home.html')
+    text = request.args.get('username')
+    if text == None: text = "User"
+    return render_template('home.html', user_template=text)
     
 if __name__ == '__main__':
     app.run(host='127.0.0.1')
