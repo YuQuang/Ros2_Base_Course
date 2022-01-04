595fb8d7ade9875bbb7f4bcc5d790e414f0c4541
Quang Xu <Quang.Xu@ad.aimmalab.com>
2022-01-04 13:34:44 +0800

    Lesson9

--- Lesson9/FlaskControl.py
+++ Lesson9/FlaskControl.py
@@ -1,25 +1,25 @@
 from flask import Flask, render_template
-# import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(14, GPIO.OUT)
-# GPIO.output(14, GPIO.LOW)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(14, GPIO.OUT)
+GPIO.output(14, GPIO.LOW)
 
 @app.route("/")
 def index():
-    # GPIO.output(14, GPIO.LOW)
+    GPIO.output(14, GPIO.LOW)
     return render_template('hyperlink.html')
 
 @app.route("/on")
 def LED_On():
-    # GPIO.output(14, GPIO.HIGH)
+    GPIO.output(14, GPIO.HIGH)
     return render_template('hyperlink.html')
 
 @app.route("/off")
 def LED_Off():
-    # GPIO.output(14, GPIO.LOW)
+    GPIO.output(14, GPIO.LOW)
     return render_template('hyperlink.html')
 
 if __name__ == "__main__":
