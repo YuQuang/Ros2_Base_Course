c761a2b5b6ddc310bfa25b4695028d9cba8e34d1
鄭宇辰 <88073402+tcc007tc007tw@users.noreply.github.com>
2022-01-04 15:23:39 +0800

    Update 1_HelloFlask.py

--- Lesson9/1_HelloFlask.py
+++ Lesson9/1_HelloFlask.py
@@ -3,6 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    text = request.args.get('username')
+    if text == None: text = "User"
     return 'Hello Flask'
     
 if __name__ == '__main__':
